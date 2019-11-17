e6a86b5343700f47cb820b6ce0b25bb07ba6fd57
AFTorres <torreand15@gmail.com>
2019-11-17 16:55:53 -0700

    Added new schedule activity, next is to fix the select apps button. Also set time is not working as expected.

--- app/src/main/java/edu/utep/cs/cs4330/dumbphone/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/dumbphone/MainActivity.java
@@ -2,7 +2,10 @@ package edu.utep.cs.cs4330.dumbphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button newSchedule=findViewById(R.id.newScheduleButton);
+
+        newSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewSchopenNewSchedule();
+            }
+        });
+    }
+    
+    private void openNewSchopenNewSchedule(){
+        Intent intent= new Intent(this,NewScheduleActivity.class);
+        startActivity(intent);
+        
     }
 }
